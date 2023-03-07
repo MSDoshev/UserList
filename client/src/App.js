4e5e244715b7c39940fd6f1490fbc255b818c598
MSDoshev <mihaildo@outlook.com>
2023-03-07 14:44:31 +0200

    Add CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-
+import Search from "./components/Search";
+import "./App.css";
 function App() {
   return (
     <>
+    
       <Header />
 
-      <main class="main">
-
+      <main className="main">
+        <Search/>
       </main>
-      
+
       <Footer />
     </>
   );
